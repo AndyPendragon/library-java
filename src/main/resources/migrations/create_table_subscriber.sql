824c01396c5d6d01bfb9c92e2d8dcf470c8aaf32
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-11-18 23:44:46 +0300

    feat: add sql script table Subscribers

---
+++ src/main/resources/migrations/create_table_subscriber.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "Subscribers" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
+    "reference" VARCHAR(255) NOT NULL
+);
+
+INSERT INTO "Subscribers" ("name", "reference") VALUES ('John Subscriber', 'JS456');
+INSERT INTO "Subscribers" ("name", "reference") VALUES ('Alice Subscriber', 'AS789');
+INSERT INTO "Subscribers" ("name", "reference") VALUES ('Bob Subscriber', 'BS012');
