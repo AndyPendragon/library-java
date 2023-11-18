b4dd210fe11043cc519a3b208c0a7a01d0f000b6
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-11-18 23:10:56 +0300

    feat: add User entity

---
+++ src/main/java/mg/pendragon/library/entity/User.java
@@ -0,0 +1,14 @@
+package mg.pendragon.library.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    private String id;
+    private String name;
+    private String reference;
+}
